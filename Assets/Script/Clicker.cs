14eb8db864484f1cb446129a01212c49856d4d0b
Prilepskiy_AE <alexey.prilepskiy1992@gmail.com>
2026-03-10 23:01:48 +0700

    click logic

---
+++ Assets/Script/Clicker.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Progress _progress;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray,out hit)){
+                if (hit.collider.GetComponent<ClickZone>())
+                {
+                    _progress.AddClick();
+                }
+            }
+                
+            
+        }
+    }
+}
